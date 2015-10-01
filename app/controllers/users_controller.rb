4965eb8869b696487c3a7d3267d5d6f6310c3eb4
Johnny Chien <jchien922@gmail.com>
2015-10-01 00:08:29 -0700

    Log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to root_url, notice: "Success!"
+			# redirect_to root_url, notice: "Success!"
+			render plain: 'Sign up Success!'
 		else
 			render 'new'
 		end
