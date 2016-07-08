059b2bb9d5ed3795188b41852cd0ca06a6284b5a
Johnny Chien <chiachie@amazon.com>
2016-07-08 09:16:13 -0700

    fix navbar colors

--- app/assets/javascripts/navBarScrolling.js
+++ app/assets/javascripts/navBarScrolling.js
@@ -2,9 +2,9 @@ var initNavbarScrolling = function() {
   $(document).scroll(function(e){
     var scrollTop = $(document).scrollTop();
     if(scrollTop > 0){
-        $('.navbar').addClass('floating-nav');
+        $('.navbar').addClass('floating-nav').removeClass('not-floating-navbar');
     } else {
-        $('.navbar').removeClass('floating-nav');
+        $('.navbar').removeClass('floating-nav').addClass('not-floating-navbar');
     }
   });
 }
