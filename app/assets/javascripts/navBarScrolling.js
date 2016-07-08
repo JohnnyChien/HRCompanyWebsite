ec55c61b2ff9412198a1cdda8b7b7d64ab9d07c5
Johnny Chien <chiachie@amazon.com>
2016-07-07 20:14:30 -0700

    Fix bugs: jumping header text on scroll, delayed navbar appearance, about header text not in the center

--- app/assets/javascripts/navBarScrolling.js
+++ app/assets/javascripts/navBarScrolling.js
@@ -2,9 +2,9 @@ var initNavbarScrolling = function() {
   $(document).scroll(function(e){
     var scrollTop = $(document).scrollTop();
     if(scrollTop > 0){
-        $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top floating-nav');
+        $('.navbar').addClass('floating-nav');
     } else {
-        $('.navbar').removeClass('navbar-fixed-top floating-nav').addClass('navbar-static-top');
+        $('.navbar').removeClass('floating-nav');
     }
   });
 }
