464d27095aaa538c955c2df2d0214d0eb09f835a
Johnny Chien <chiachie@amazon.com>
2016-07-10 20:16:36 -0700

    demo fixes

--- app/assets/javascripts/navBarScrolling.js
+++ app/assets/javascripts/navBarScrolling.js
@@ -3,8 +3,12 @@ var initNavbarScrolling = function() {
     var scrollTop = $(document).scrollTop();
     if(scrollTop > 0){
         $('.navbar').addClass('floating-nav').removeClass('not-floating-navbar');
+        $('#logo-text').addClass('floating-text').removeClass('not-floating-text');
+        $('#logo-img').attr('src', 'assets/logo.png');
     } else {
         $('.navbar').removeClass('floating-nav').addClass('not-floating-navbar');
+        $('#logo-text').removeClass('floating-text').addClass('not-floating-text');
+        $('#logo-img').attr('src', 'assets/logo_white.png');
     }
   });
 }
