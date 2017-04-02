27bef4fe07bb8d901044a64f97f8681930deaf28
Johnny Chien <chiachie@amazon.com>
2017-04-01 17:59:55 -0700

    Floating navbar behaves correctly.

--- app/assets/javascripts/navBarScrolling.js
+++ app/assets/javascripts/navBarScrolling.js
@@ -2,11 +2,13 @@ var initNavbarScrolling = function() {
   $(document).scroll(function(e){
     var scrollTop = $(document).scrollTop();
     if(scrollTop > 0){
-        $('.navbar-nav').addClass('floating-nav').removeClass('not-floating-navbar');
+        $('nav.navbar').addClass('floating-nav').removeClass('not-floating-navbar');
+        $('ul.navbar-nav').addClass('floating-nav').removeClass('not-floating-navbar');
         $('#logo-text').addClass('floating-text').removeClass('not-floating-text');
         $('#logo-img').attr('src', 'assets/logo.png');
     } else {
-        $('.navbar-nav').removeClass('floating-nav').addClass('not-floating-navbar');
+        $('nav.navbar').removeClass('floating-nav').addClass('not-floating-navbar');
+        $('ul.navbar-nav').removeClass('floating-nav').addClass('not-floating-navbar');
         $('#logo-text').removeClass('floating-text').addClass('not-floating-text');
         $('#logo-img').attr('src', 'assets/logo_white.png');
     }
