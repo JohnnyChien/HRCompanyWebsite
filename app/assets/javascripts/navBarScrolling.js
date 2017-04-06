6d4bf37f62be913f3f93f75f1096208ba1af19bd
Johnny Chien <chiachie@amazon.com>
2017-04-05 18:38:18 -0700

    Improvement on navbar hamburger menu, three-border-button and font family

--- app/assets/javascripts/navBarScrolling.js
+++ app/assets/javascripts/navBarScrolling.js
@@ -5,11 +5,13 @@ var initNavbarScrolling = function() {
         $('nav.navbar').addClass('floating-nav').removeClass('not-floating-navbar');
         $('ul.navbar-nav').addClass('floating-nav').removeClass('not-floating-navbar');
         $('#logo-text').addClass('floating-text').removeClass('not-floating-text');
+        $('.navbar-default .navbar-toggle .icon-bar').removeClass('not-floating').addClass('floating');
         $('#logo-img').attr('src', 'assets/logo.png');
     } else {
         $('nav.navbar').removeClass('floating-nav').addClass('not-floating-navbar');
         $('ul.navbar-nav').removeClass('floating-nav').addClass('not-floating-navbar');
         $('#logo-text').removeClass('floating-text').addClass('not-floating-text');
+        $('.navbar-default .navbar-toggle .icon-bar').removeClass('floating').addClass('not-floating');
         $('#logo-img').attr('src', 'assets/logo_white.png');
     }
   });
